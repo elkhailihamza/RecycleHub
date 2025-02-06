bfd34515c664806133c7be8e51d9a3d814458e7c
elkhailihamza <elkhailihamza331@gmail.com>
2025-02-06 09:14:08 +0100

    gave styles to navbar

--- recycle-hub/recycle-hub-front-end/recycle-hub/src/app/core/shared/layout/layout/navbar/navbar.component.ts
+++ recycle-hub/recycle-hub-front-end/recycle-hub/src/app/core/shared/layout/layout/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IsActiveMatchOptions, Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
@@ -6,5 +7,19 @@ import { Component } from '@angular/core';
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
+
+  constructor(private router: Router) {
+
+  }
+
+  isRouteActive(url: string): boolean {
+    const matchOptions: IsActiveMatchOptions = {
+      paths: 'exact',
+      queryParams: 'ignored',
+      fragment: 'ignored',
+      matrixParams: 'ignored',
+    };
+    return this.router.isActive(url, matchOptions);
+  }
 
 }
