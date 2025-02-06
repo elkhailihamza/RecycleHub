8520f7b908d0a9da3ec084de7f50413c0b5160d9
elkhailihamza <elkhailihamza331@gmail.com>
2025-02-06 16:30:16 +0100

    added index page for more information on how to contribute and request

--- recycle-hub/recycle-hub-front-end/recycle-hub/src/app/core/contribute/contribute.module.ts
+++ recycle-hub/recycle-hub-front-end/recycle-hub/src/app/core/contribute/contribute.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RequestComponent } from './request/request.component';
 import { ContributeRoutingModule } from './contribute-routing.module';
+import { IndexComponent } from './index/index.component';
 
 @NgModule({
   declarations: [
-    RequestComponent
+    RequestComponent,
+    IndexComponent
   ],
   imports: [
     CommonModule,
